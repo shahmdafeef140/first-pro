a7e056247299ac0b9ad44337a4cbbe86158a7d0f
shahmdafeef140 <shahmdafeef5@gmail.com>
2026-09-12 20:21:26 +0600

    Add project files

--- first.py
+++
@@ -1,7 +0,0 @@
-const student = {
-  name: "John Doe",
-  age: 20,
-  major: "Computer Science",
-    greet: function() {
-    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-    }
